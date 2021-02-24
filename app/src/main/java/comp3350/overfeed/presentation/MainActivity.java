ebfd66d6def6a7d9e2c3daba4b0f48b26d9d79fd
Dan <luznyd@myumanitoba.ca>
2021-02-24 05:58:23 -0600

    timer created

---
+++ app/src/main/java/comp3350/overfeed/presentation/MainActivity.java
@@ -0,0 +1,43 @@
+package comp3350.overfeed.presentation;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.app.Activity;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import comp3350.overfeed.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    // Timer set up
+    TextView timerTextView;
+    long startTime = System.currentTimeMillis();
+
+    Handler timerHandler = new Handler();
+    Runnable timerRunnable = new Runnable()
+    {
+        @Override
+        public void run()
+        {
+            int seconds = (int)((System.currentTimeMillis() - startTime)/1000); // divide by 1000 here because 1000ms == 1s
+            int minutes = seconds / 60;
+
+            timerTextView.setText(String.format("%d:%02d", minutes, seconds));
+
+            timerHandler.postDelayed(this, 500);
+        }
+    }; // End timer set up
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        timerTextView = (TextView)findViewById(R.id.timerTextView);
+        timerHandler.postDelayed(timerRunnable, 0);
+
+    }
+}
