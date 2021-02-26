04b90c5fbc58655d29a8803d34d41efc5ba38ba7
Chris Ciceron <christopherciceron@gmail.com>
2021-02-25 22:01:54 -0600

    Updated content related to Statistics Activity

---
+++ app/src/main/java/comp3350/overfeed/presentation/StatisticsActivity.java
@@ -0,0 +1,45 @@
+package comp3350.overfeed.presentation;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import comp3350.overfeed.R;
+
+public class StatisticsActivity extends AppCompatActivity {
+
+    TextView textTotalClicks;
+    TextView textCurrentTime;
+    TextView textTotalMeals;
+    TextView textTotalTime;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_statistics);
+
+        Intent mainIntent = getIntent();
+        Bundle extras = mainIntent.getExtras();
+
+        String clicks_string = extras.getString("NUMBER_CLICKS");
+        String minutes_string = extras.getString("TIME_MINUTES");
+        String seconds_string = extras.getString("TIME_SECONDS");
+
+        textTotalClicks = (TextView)findViewById(R.id.textTotalClicks);
+        textCurrentTime = (TextView)findViewById(R.id.textCurrentTime);
+        textTotalMeals = (TextView)findViewById(R.id.textTotalMeals);
+        textTotalTime = (TextView)findViewById(R.id.textTotalTime);
+    }
+
+    public void buttonBackHomeOnClick(View view)
+    {
+        Intent backHomeIntent = new Intent(StatisticsActivity.this, MainActivity.class);
+        StatisticsActivity.this.startActivity(backHomeIntent);
+    }
+
+}
+
