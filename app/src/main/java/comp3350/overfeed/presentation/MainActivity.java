52b378bdf88361285bd0e280e45b1824f87cc1e5
Dan <luznyd@myumanitoba.ca>
2021-03-19 11:31:52 -0500

    Made text boxes on achievements page bigger.
    Moved the "check for achievement" into a thread so it does it automatically

--- app/src/main/java/comp3350/overfeed/presentation/MainActivity.java
+++ app/src/main/java/comp3350/overfeed/presentation/MainActivity.java
@@ -36,6 +36,13 @@ public class MainActivity extends AppCompatActivity {
                 mealLogic.autoIncreaseMeals();
                 counterTextView.setText(mealLogic.mealsToString());
             }
+
+            if(achLogic.checkClickAchievement(mealLogic.getMeals())){
+                String newAchievement = achLogic.getNewAchievement().getName();
+                Snackbar mySnackbar = Snackbar.make(findViewById(R.id.myCoordinatorLayout), "Achievement Unlocked! "+ newAchievement, Snackbar.LENGTH_SHORT);
+                mySnackbar.show();
+            }
+
             mealHandler.postDelayed(this, 1000); // 1000 here because we want the counter to update every second(1000ms). Upgrades are on a per-second timer.
         }
     };
@@ -106,12 +113,6 @@ public class MainActivity extends AppCompatActivity {
         mealLogic.increaseMeals();
         counterTextView = (TextView)findViewById(R.id.counterView);
         counterTextView.setText(mealLogic.mealsToString());
-
-        if(achLogic.checkClickAchievement(mealLogic.getMeals())){
-            String newAchievement = achLogic.getNewAchievement().getName();
-            Snackbar mySnackbar = Snackbar.make(findViewById(R.id.myCoordinatorLayout), "Achievement Unlocked! "+ newAchievement, Snackbar.LENGTH_SHORT);
-            mySnackbar.show();
-        }
     }
 
     //     OnClick methods for Tab Items (Statistics, Achievements)
